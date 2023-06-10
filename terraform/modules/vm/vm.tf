a5fedfa37769f50c7bb6918f982c31bd389733a4
Ionut Munteanu <ionut.munteanu@bertelsmann.de>
2023-06-10 23:51:44 +0300

    use file for public key

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = "azureDevOps"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDk/ibfJgffN/APLolfkP2f5VFKlppF7mjCcSY+6wNkA9LKeY3Y0/TWBlUDHDiX5pVcRxDBWXt4CMhDCYiP94DEsci3Ma7Jo27ZFtXQ3yjJtEGO3oDQ9nkDRMj5YTX4MXGgnNa85QuaanlaoAFtIcVWT73vIl0XmpmSXOOmoGoWly2+wnqbglQgrp6ZdLsmI3Me0UEudTsDxvrzfBm+w+e/UFgNN0Agjqxwd/I18zXx8ttp9xPr25siUyjv0fuhWrP2CQX8p++iSIGiqSS3SKMumHkzCBA5XGLHZk+cv65a9OAFQBT5FgQZGbDfx8enNpcF7dE/LgdRZdvzP2TeX32Fvt5dEXu+ONx7Kg99jAls+pQDcg1CTstlcQ4HrDteNOhXPb+HpCrtbIufClOp4EDLKqPlxDuz2QwNvSEHx79QP2KOVzu5cU0/6qASZRbIk9RxhQHldIGasc0ST3Ublpx1+nsmolvUd1G8hKXpKZGcYfmYtv+BzvKLLels6NNJxMM= BGROUP+MUNT043@BGPF3J0LQC"
+    public_key = "file("~/.ssh/id_rsa.pub")"
   }
   os_disk {
     caching           = "ReadWrite"
